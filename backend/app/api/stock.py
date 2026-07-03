75fcc8cf7b419fef7b4bd271ccea7fb4017cbba4
Vishaal Ravichandran <vishaalravichandran369@gmail.com>
2026-07-03 13:04:33 +0530

    Implement historical market data service

--- backend/app/api/stock.py
+++ backend/app/api/stock.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.services.market_service import market_service
+from fastapi import APIRouter, Query
 
 router = APIRouter(
     prefix="/stock",
@@ -10,3 +11,10 @@ router = APIRouter(
 @router.get("/{ticker}")
 def get_stock(ticker: str):
     return market_service.get_stock_info(ticker)
+
+@router.get("/{ticker}/history")
+def get_stock_history(ticker: str,period: str = Query(default="6mo")):
+    
+    history = market_service.get_historical_data(ticker, period)
+
+    return history.reset_index().to_dict(orient="records")
--- backend/app/services/market_service.py
+++ backend/app/services/market_service.py
@@ -23,6 +23,20 @@ class MarketService:
             "high_52_week": info.get("fiftyTwoWeekHigh"),
             "low_52_week": info.get("fiftyTwoWeekLow")
         }
+    
+    def get_historical_data(self, ticker: str, period: str = "6mo"):
+        stock = yf.Ticker(ticker)
 
+        history = stock.history(period=period)
+
+        if history.empty:
+            raise HTTPException(
+                status_code=404,
+                detail=f"No historical data found for '{ticker.upper()}'."
+            )
+
+        return history[
+            ["Open", "High", "Low", "Close", "Volume"]
+        ]
 
 market_service = MarketService()
